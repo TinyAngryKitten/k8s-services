55697f6cff1423da00dd2e6395fb602299426efd
Sander Jokiel Høyvik <sander.hoyvik@gmail.com>
2023-04-05 13:31:06 +0200

    Change config files

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,4 +1,4 @@
-imagename=tinyangrykitten/template:latest
+imagename=tinyangrykitten/http-mqtt:latest
 ./gradlew build -Dquarkus.package.type=native
 
 docker build -f src/main/docker/Dockerfile.native-micro -t $imagename --platform linux/amd64 .
